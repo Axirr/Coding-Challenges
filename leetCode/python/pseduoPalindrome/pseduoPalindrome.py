eda11799842245ff81a91ba35710f92a6d59f3b5
Axirr <sherlock.scott@gmail.com>
2022-09-14 14:44:54 -0600

    Much faster pseduoPalindrome solution.

--- leetCode/python/pseduoPalindrome/pseduoPalindrome.py
+++ leetCode/python/pseduoPalindrome/pseduoPalindrome.py
@@ -16,13 +16,22 @@ class TreeNode:
 
 class Solution:
     def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
-        countDict = {}
-        for num in range(1, 9 + 1):
-            countDict[num] = True
-        return self.countPseudoPalindrome(root, countDict, True)
+        badNumbers = ""
+        # countDict = {}
+        # for num in range(1, 9 + 1):
+        #     countDict[num] = True
+        return self.countPseudoPalindrome(root, badNumbers, True)
 
-    def countPseudoPalindrome(self, root: Optional[TreeNode], countDict, isOdd):
-        countDict[root.val] = not countDict[root.val]
+    def countPseudoPalindrome(self, root: Optional[TreeNode], badNumbers, isOdd):
+        myValue = str(root.val)
+        doAdd = True
+        for i, value in enumerate(badNumbers):
+            if (value == myValue):
+                badNumbers = badNumbers[:i] + badNumbers[i+1:]
+                doAdd = False
+                break
+        if (doAdd):
+            badNumbers += myValue
         count = 0
 
         # Bottom of a path, check if palindrome possible
@@ -30,18 +39,19 @@ class Solution:
             if (isOdd):
                 falseCount = 1
             else:   falseCount = 0
-            for key in countDict.keys():
-                if (countDict[key] == False):
-                    falseCount -= 1
-                    if (falseCount < 0):
-                        return count
-            count += 1
+            if (len(badNumbers) <= falseCount):
+                count += 1
+            # for key in countDict.keys():
+            #     if (countDict[key] == False):
+            #         falseCount -= 1
+            #         if (falseCount < 0):
+            #             return count
         # Not bottom of a path
         else:
             if (root.left):
-                count += self.countPseudoPalindrome(root.left, countDict.copy(), not isOdd)
+                count += self.countPseudoPalindrome(root.left, badNumbers, not isOdd)
             if (root.right):
-                count += self.countPseudoPalindrome(root.right, countDict.copy(), not isOdd)
+                count += self.countPseudoPalindrome(root.right, badNumbers, not isOdd)
         return count
         '''
         Naive: construct list for each path using depth first method
@@ -54,11 +64,12 @@ class Solution:
         '''
 
 def main():
-    # myTree = constructTreeFromList([2,3,1,3,1,None,1])
-    # myTree = constructTreeFromList([2,1,1,1,3,None,None,None,None,None,1])
-    myTree = constructTreeFromList([9])
-    # myTree.recursivePrint()
     mySolution = Solution()
+    myTree = constructTreeFromList([2,3,1,3,1,None,1])
+    print(mySolution.pseudoPalindromicPaths(myTree))
+    myTree = constructTreeFromList([2,1,1,1,3,None,None,None,None,None,1])
+    print(mySolution.pseudoPalindromicPaths(myTree))
+    myTree = constructTreeFromList([9])
     print(mySolution.pseudoPalindromicPaths(myTree))
 
 def constructTreeFromList(nodeList):
