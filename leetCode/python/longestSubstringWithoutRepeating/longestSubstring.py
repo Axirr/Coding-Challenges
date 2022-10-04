03d010fe5be35366bf1d193a22bcdaa87e3d150d
Axirr <sherlock.scott@gmail.com>
2022-10-04 11:06:33 -0600

    Solution for Longest Substring with no Repeats, in Python.

--- leetCode/python/longestSubstringWithoutRepeating/longestSubstring.py
+++ leetCode/python/longestSubstringWithoutRepeating/longestSubstring.py
@@ -2,71 +2,140 @@ import string
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        '''
-        Data range/assumptions
-        Strings can be 0
-        Strings can be very long
-        '''
+        if s == "":     return 0
 
-        '''
-        Test cases
-        Empty string
-        Single character
-        No duplicates -> whole string
-        Duplicate letters are first and last
-        '''
-
-        '''
-        Plain language:
-        Strings this long, naive n^2 isn't going to do it
-        Dynamic programming
-            Halving doesn't work?
-                Because sum of substring halves might not be right
-                E.g. "
-            Extend by 1?
-                substring(start, finish + 1) = substring(start, finish) + substring(finsish, finish + 1)
-        Count dictionary?
-            But then we know not valid, but where to break
-                E.g. abcdefgha would only be shorted by 1, but bcdefghaa would use the front
-        Recursive:
-            Basic case: single letter, whole thing
-            Extend left and right?
-        Cases when duplicate found
-            Include original, duplicate + 1 now one boundary
-            Include duplicate, original - 1 now one boundary
-        But what of case like 123456788988....
-            If we started on 9, we would find duplicates both directions
-            But longest would be the start part
-        So can't just start in middle
-        Have to do all n letters, extending until we find a duplicate
-            Worst case: duplicates are front and back, so everything extends n -> n ^ 2
-        Traverse string counting duplicates, keeping index
-            Construct span for each letter, duplicate to duplicate
-            Each one of those is a conditon that needs to be met if in relevant index range
-                E.g. is [i, j] a valid substring
-                    Yes if not duplicate a, b, c...
-                if startRange in [i,j] and endRange in [i,j]
-                    that's a duplicate
-                Starting with big range is bad idea
-                    E.g. if can't find a 2 ranger, obviously can't find a 2+ ranger
-                But incrementing bad too
-                    Binary mid?
-                    If half not possible
-                        Possible range: [half - 1, 1]
-                    If half possible:
-                        Possible range: [all, half + 1]
-                    Search mid of possible range, cut range down accordingly
-            Keep track of minLength of all characters
-                That's maximum length, shouldn't start past that
-        Worst case here
-            All duplicates -> spans same length as string
-        '''
-
-        if s == "":     return ""
-
-        currentIndex = [-1] * 26
-        validChars = list(string.ascii_letters + string.digits + string.punctuation)
+        # validLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ \n"
+        # print(len(validLetters))
+        numSymbols = 191
+        maxSubstringLen = 1
+        count = 1
         for i in range(len(s)):
-            letter = s[i]
-            letterIndex = validChars.indexof(letter)
+            letterString = s[i]
+            count = 1
+            didBreak = False
+            for j in range(i + 1, min(i + 1 + numSymbols, len(s))):
+                if s[j] in letterString:
+                    maxSubstringLen = max(count, maxSubstringLen)
+                    didBreak = True
+                    break
+                letterString += s[j]
+                count += 1
+            if (not didBreak):
+                maxSubstringLen = max(count, maxSubstringLen)
+            if (maxSubstringLen >= numSymbols): return maxSubstringLen
+        # maxSubstringLen = max(count, maxSubstringLen)
+        return maxSubstringLen
+
+def main():
+    mySolution = Solution()
+    myString = "hello"
+    resultCount = mySolution.lengthOfLongestSubstring(myString)
+    assert resultCount == 3
+    print(resultCount)
+    myString = "abcabcbb"
+    resultCount = mySolution.lengthOfLongestSubstring(myString)
+    assert resultCount == 3
+    print(resultCount)
+    myString = "bbbbb"
+    resultCount = mySolution.lengthOfLongestSubstring(myString)
+    assert resultCount == 1
+    print(resultCount)
+    myString = "pwwkew"
+    resultCount = mySolution.lengthOfLongestSubstring(myString)
+    assert resultCount == 3
+    print(resultCount)
+    myString = "au"
+    resultCount = mySolution.lengthOfLongestSubstring(myString)
+    print(resultCount)
+    assert resultCount == 2
+
+main()
+
+'''
+Data range/assumptions
+Strings can be 0
+Strings can be very long
+Valid digits are alpha + num + symbolsa + space
+    Len defines longest possible non-repeating substring
+'''
+
+'''
+Test cases
+Empty string
+Single character
+No duplicates -> whole string
+Duplicate letters are first and last
+All duplicates
+'''
+
+'''
+Plain language:
+Strings this long, naive n^2 isn't going to do it
+Dynamic programming
+    Halving doesn't work?
+        Because sum of substring halves might not be right
+        E.g. "
+    Extend by 1?
+        substring(start, finish + 1) = substring(start, finish) + substring(finsish, finish + 1)
+Count dictionary?
+    But then we know not valid, but where to break
+        E.g. abcdefgha would only be shorted by 1, but bcdefghaa would use the front
+Recursive:
+    Basic case: single letter, whole thing
+    Extend left and right?
+Cases when duplicate found
+    Include original, duplicate + 1 now one boundary
+    Include duplicate, original - 1 now one boundary
+But what of case like 123456788988....
+    If we started on 9, we would find duplicates both directions
+    But longest would be the start part
+So can't just start in middle
+Have to do all n letters, extending until we find a duplicate
+    Worst case: duplicates are front and back, so everything extends n -> n ^ 2
+Traverse string counting duplicates, keeping index
+    Construct span for each letter, duplicate to duplicate
+    Each one of those is a conditon that needs to be met if in relevant index range
+        E.g. is [i, j] a valid substring
+            Yes if not duplicate a, b, c...
+        if startRange in [i,j] and endRange in [i,j]
+            that's a duplicate
+        Starting with big range is bad idea
+            E.g. if can't find a 2 ranger, obviously can't find a 2+ ranger
+        But incrementing bad too
+            Binary mid?
+            If half not possible
+                Possible range: [half - 1, 1]
+            If half possible:
+                Possible range: [all, half + 1]
+            Search mid of possible range, cut range down accordingly
+    Keep track of minLength of all characters
+        That's maximum length, shouldn't start past that
+Worst case here
+    All duplicates -> spans same length as string
+
+Letter by letter span isn't worst case of n ^ 2
+    Maximum valid string can only be ~40 in length
+    So it would be 40 n, not n ^ 2
+    Substantially different
+    Doable at this scale I think
+
+Plain language:
+    maxSubstringLen = 1
+    for i in range(len(myStr)):
+        letterString = myStr[i]
+        count = 0
+        for j in range(i + 1, min(i + 40, len(myStr)):
+            if myStr(j) in letterString:
+                maxSubstringLen = max(count, maxSubstringLen)
+                break
+        if (maxSubstringLen >= 40): return maxSubstringLen
+
+    Sanity check:
+        Time complexity alright? Yes, should be n 40
+        Works?
+            Will test all possible valid substrings
+            Will break early if found theoretical max
+
+
+'''
 
