04cdd1611787a877c155d2aa49e080b10c84523b
Axirr <sherlock.scott@gmail.com>
2022-11-08 10:15:53 -0700

    Solution for Make String Great, in Python.

---
+++ leetCode/python/makeStringGreat/makeStringGreat.py
@@ -0,0 +1,45 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        i = 0
+        while (i < len(s) - 1):
+            first = s[i]
+            second = s[i+1]
+            if first.lower() == second.lower():
+                if (first.isupper() and second.islower()) or (first.islower() and second.isupper()):
+                    s = s[:i] + s[i+2:]
+                    i = 0
+                    continue
+            i += 1
+        return s
+
+
+def main():
+    mySol = Solution()
+    myString = 'leEeetcode'
+    s = mySol.makeGood(myString)
+    print(s)
+
+main()
+
+'''
+Data range/assumptions:
+Non-empty
+    Empty result allowed
+Short
+Only lower and upper case letters
+'''
+
+'''
+Tests:
+Single char
+All bad?
+'''
+
+'''
+Ideas:
+
+Naive:
+    Traverse string to second last
+    Check if self and neighbour are opposite capital
+    Remove if so
+'''
