81678ba401b3c01a721c9ba2951df8f5f8c4ff34
Axirr <sherlock.scott@gmail.com>
2022-11-19 10:43:50 -0700

    Improved solution to Exchange Seats, in MySQL.

--- leetCode/mySql/swapConsecutiveSeats.sql
+++ leetCode/mySql/swapConsecutiveSeats.sql
@@ -1,7 +1,13 @@
 # Write your MySQL query statement below
 
-select * from (select S2.id as id, S1.student as student from Seat S1 right join Seat S2 on S1.id + 1 = S2.id where mod(S2.id, 2) = 0
+select * from (select id+1 as id, student from Seat where mod(id, 2) = 1 and id not in (select max(id) from Seat)
 union
-select S1.id as id, S2.student as student from Seat S1 left join Seat S2 on S1.id + 1 = S2.id where mod(S1.id, 2) = 1 and S1.id not in (select max(id) from Seat)
+select id-1 as id, student from Seat where mod(id, 2) = 0
 union
-select * from (select id, student from Seat order by id desc limit 1) as B where mod(id, 2) = 1) as A order by id;
+select id, student from Seat where id in (select max(id) from Seat) and mod(id, 2) = 1) as A order by id;
+
+-- select * from (select S2.id as id, S1.student as student from Seat S1 right join Seat S2 on S1.id + 1 = S2.id where mod(S2.id, 2) = 0
+-- union
+-- select S1.id as id, S2.student as student from Seat S1 left join Seat S2 on S1.id + 1 = S2.id where mod(S1.id, 2) = 1 and S1.id not in (select max(id) from Seat)
+-- union
+-- select * from (select id, student from Seat order by id desc limit 1) as B where mod(id, 2) = 1) as A order by id;
