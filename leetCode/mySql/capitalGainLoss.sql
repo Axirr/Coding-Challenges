aa23d98a373afca829ddfa4f9504363e8ff6eb2e
Axirr <sherlock.scott@gmail.com>
2022-11-28 10:41:45 -0700

    Third solution for Capital Gain/Loss, in MySQL.

--- leetCode/mySql/capitalGainLoss.sql
+++ leetCode/mySql/capitalGainLoss.sql
@@ -2,9 +2,11 @@
 
 # Write your MySQL query statement below
 
-select A.stock_name, B.mySum - A.mySum as capital_gain_loss from (select stock_name, sum(price) as mySum from Stocks where operation = "Buy" group by stock_name) as A
-left join
-(select stock_name, sum(price) as mySum from Stocks where operation = "Sell" group by stock_name) as B on A.stock_name = B.stock_name;
+select stock_name, sum(if(operation = "Buy", -price, price)) as capital_gain_loss from Stocks group by stock_name;
+
+-- select A.stock_name, B.mySum - A.mySum as capital_gain_loss from (select stock_name, sum(price) as mySum from Stocks where operation = "Buy" group by stock_name) as A
+-- left join
+-- (select stock_name, sum(price) as mySum from Stocks where operation = "Sell" group by stock_name) as B on A.stock_name = B.stock_name;
 
 -- select A.stock_name as stock_name, sum(A.price) - sum(B.price) as capital_gain_loss from (select row_number() over (order by stock_name, operation_day) myRow,
 --         stock_name, operation_day, price
