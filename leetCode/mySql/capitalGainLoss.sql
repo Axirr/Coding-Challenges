97e749d64f622a9351ed35a98916818e6ad076f1
Axirr <sherlock.scott@gmail.com>
2022-11-28 10:38:50 -0700

    Improved solution for Capital Gain/Loss, in MySQL.

--- leetCode/mySql/capitalGainLoss.sql
+++ leetCode/mySql/capitalGainLoss.sql
@@ -1,11 +1,15 @@
 # Write your MySQL query statement below
 
-# select * from Stocks S1 left join Stocks S2 on S1.operation = "Buy" and S2.operation = "Sell" and S2.operation_day >= S1.operation_day order by S2.operation_day desc;
+# Write your MySQL query statement below
 
-select A.stock_name as stock_name, sum(A.price) - sum(B.price) as capital_gain_loss from (select row_number() over (order by stock_name, operation_day) myRow,
-        stock_name, operation_day, price
-from Stocks where operation = "Sell") as A
+select A.stock_name, B.mySum - A.mySum as capital_gain_loss from (select stock_name, sum(price) as mySum from Stocks where operation = "Buy" group by stock_name) as A
 left join
-(select row_number() over (order by stock_name, operation_day) myRow,
-        stock_name, operation_day, price
-from Stocks where operation = "Buy") as B on A.myRow = B.myRow group by A.stock_name;
+(select stock_name, sum(price) as mySum from Stocks where operation = "Sell" group by stock_name) as B on A.stock_name = B.stock_name;
+
+-- select A.stock_name as stock_name, sum(A.price) - sum(B.price) as capital_gain_loss from (select row_number() over (order by stock_name, operation_day) myRow,
+--         stock_name, operation_day, price
+-- from Stocks where operation = "Sell") as A
+-- left join
+-- (select row_number() over (order by stock_name, operation_day) myRow,
+--         stock_name, operation_day, price
+-- from Stocks where operation = "Buy") as B on A.myRow = B.myRow group by A.stock_name;
