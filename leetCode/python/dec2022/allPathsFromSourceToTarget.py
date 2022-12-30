015e9c286f0852918891231be2e2db8bc8ed8453
Axirr <sherlock.scott@gmail.com>
2022-12-30 11:29:49 -0700

    Alternate solution for All Paths From Source to Target, in Python.

--- leetCode/python/dec2022/allPathsFromSourceToTarget.py
+++ leetCode/python/dec2022/allPathsFromSourceToTarget.py
@@ -1,10 +1,37 @@
 from typing import List
+from copy import copy
 
 class Solution:
+    # Class variables used in recursive method
     graph = None
     target = None
     cache = {}
+
+    # Dynamic programming top down
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        targetIndex = len(graph) - 1
+        cache = {}
+        frontier = [[0]]
+        solutions = []
+        while (frontier):
+            currPath = frontier.pop()
+            endCurrPath = currPath[-1]
+            for node in graph[endCurrPath]:
+                copyPath = copy(currPath)
+                if node == targetIndex:
+                    copyPath.append(node)
+                    solutions.append(copyPath)
+                    cache[copyPath[0]] = copyPath
+                else:
+                    if node in cache:
+                        solutions.append(copyPath + cache[node])
+                    else:
+                        copyPath.append(node)
+                        frontier.append(copyPath)
+        return solutions
+
+    # Recursive with memoization
+    def recursiveAllPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         self.graph = graph
         self.target = len(graph) - 1
         self.cache = {}
@@ -28,12 +55,16 @@ def main():
     sol = Solution()
     graph = [[1,2],[3],[3],[]]
     correctOutput = [[0,1,3],[0,2,3]]
+    correctOutput.sort()
     actualOutput = sol.allPathsSourceTarget(graph)
+    actualOutput.sort()
     print(actualOutput)
     assert actualOutput == correctOutput
     graph = [[4,3,1],[3,2,4],[3],[4],[]]
     correctOutput = [[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]
+    correctOutput.sort()
     actualOutput = sol.allPathsSourceTarget(graph)
+    actualOutput.sort()
     print(actualOutput)
     assert actualOutput == correctOutput
 
@@ -73,4 +104,18 @@ Will get duplicates?
 Better: build out from target so don't have to duplicate work
     Then when hit node that has been visited before, know path
     Recursive with memoization
+
+Time complexity:
+    Will traverse each edge exactly once
+    Otherwise will use cached version
+    O(e), assuming all nodes are accessible
+
+All edges will have to be visited, so bottom up could be more efficient
+Restructuring as bottom up dynamic programming:
+    But bottom up is difficult here because edges are directed
+    Given minimal advantages of bottom up versus top down, top down seems better here
+
+What about top down non-recursive?
+
+If build tree by height, then can create lists from those heights, stopping everytime we hit target
 '''
