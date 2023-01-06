323ef1a47a5b2eebbb99c2527162fa6ff3b85b4a
Axirr <sherlock.scott@gmail.com>
2023-01-06 11:43:40 -0700

    Improved solution for Maximum Ice Cream Bars, in Python.

--- leetCode/python/jan2023/maxIceCream.py
+++ leetCode/python/jan2023/maxIceCream.py
@@ -1,16 +1,42 @@
 from typing import List
+from heapq import *
+from collections import Counter
 
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
-        costs.sort()
-        i = 0
-        costSum = 0
+        # Unclear if using a hash table but then having to sort by keys is 
+        #   more efficient than initializing a list to 0 for all values
+        #      n + m        vs.     n + klogk + k   where k = # of distinct costs
+        count = Counter()
         for i in range(len(costs)):
-            costSum += costs[i]
-            if costSum > coins:
-                i -= 1
+            count[costs[i]] += 1
+        totalCost = 0
+        totalCount = 0
+        for i in sorted(count.keys()):
+            totalCost += i * count[i]
+            totalCount += count[i]
+            if totalCost > coins:
+                while totalCost > coins:
+                    totalCost -= i
+                    totalCount -= 1
                 break
-        return max(0, i + 1)
+        return totalCount
+
+    # Not sure if there is merit to this method over just sorting
+    def heapMaxIceCream(self, costs: List[int], coins: int) -> int:
+        myMaxHeap = []
+        totalCost = 0
+        totalCount = 0
+        negativeCoins = -coins
+        for i in range(len(costs)):
+            currentCost = -costs[i]
+            if totalCost + currentCost >= negativeCoins:
+                heappush(myMaxHeap, currentCost)
+                totalCost += currentCost
+                totalCount += 1
+            elif len(myMaxHeap) > 0 and myMaxHeap[0] < currentCost:
+                totalCost += -heapreplace(myMaxHeap, currentCost) + currentCost
+        return totalCount
 
 def main():
     sol = Solution()
@@ -24,6 +50,11 @@ def main():
     maxBars = sol.maxIceCream(costs, coins)
     print(maxBars)
     assert maxBars == 6
+    costs = [10,2,10,10,10,10,8,2,7,8]
+    coins = 25
+    maxBars = sol.maxIceCream(costs, coins)
+    print(maxBars)
+    # assert maxBars == 4
 
 main()
 
@@ -50,4 +81,18 @@ Ideas:
 
 Naive:
     sort in ascending order and sum costs until sum greater than coin
+
+Better: max heap
+    Traverse costs, maintaining a max heap and keeping track of total sum of maxHeap
+    If adding next bar would exceed coins
+        Pop and push if max of heap is larger than current
+        Updating current cost
+    Return max heap size
+
+    Time complexity:
+        Single traversal
+        But worst case, have to do a sift up operation each time
+            I.e. descending costs
+            nlogn
+        Average case might be better?
 '''
