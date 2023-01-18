9d521967ebf57bd78721402c7a3e7fc9d1b69151
Axirr <sherlock.scott@gmail.com>
2023-01-18 13:25:40 -0700

    Improved solution for Maximum Subarray, in Typescript.

--- leetCode/typescript/maximumSubArray.ts
+++ leetCode/typescript/maximumSubArray.ts
@@ -1,15 +1,40 @@
 function maxSubArray(nums: number[]): number {
-    var lastIndex:number = nums.length - 1
-    var maxSum:number = nums[lastIndex]
-    var lastValue = nums[lastIndex]
+    var maxSum:number = nums[0]
+    var partialRunningSum:number = nums[0]
 
-    for (var i = lastIndex - 1; i >= 0; i--) {
-        lastValue = lastValue + nums[i]
-        if (nums[i] > lastValue) {
-            lastValue = nums[i]
+    for (var i = 1; i < nums.length; i++) {
+        partialRunningSum = partialRunningSum + nums[i]
+        if (nums[i] > partialRunningSum) {
+            // Starting subArray and partialRunningSum from this index
+            // since it is better than whole current subArray
+            partialRunningSum = nums[i]
         }
-        maxSum = Math.max(maxSum, lastValue)
+        maxSum = Math.max(maxSum, partialRunningSum)
     }
 
     return maxSum
 };
+
+// Example where maxSum and partialRunningSum will be tracking arrays with different start points at i = 2
+var nums:number[] = [10,-100,9,2]
+var maxSum:number = maxSubArray(nums)
+console.log(maxSum)
+console.assert(maxSum === 11)
+
+/*
+How does this algorithm ensure optimality?
+    Last value is a running sum
+    Only pick a new starting point for the subArray if that number is greater than running sum from last starting point
+    We update last value even if it isn't a new max
+
+So we're keeping track of two things:
+    currentMax for the ith index
+    sum array if we took everything from our last optimal starting point
+    currentMax is greedy, but 
+
+Note: partialRunningSum and maxSum don't necessarily have the same starting index for their subarray
+    E.g. [10, -100, 9, 2]
+        At i = 2, partialRunningSum will reset to 9
+        But maxSum will still be at 10
+        At i = 3, partialRunningSum of 11 will become newMax
+*/
