a9be2d949a9cc2b63a2f58bad15007f59ff33c5a
Axirr <sherlock.scott@gmail.com>
2023-01-24 13:48:41 -0700

    Improved solution for Snakes and Ladders, in Python.

--- leetCode/python/jan2023/snakesAndLadders.py
+++ leetCode/python/jan2023/snakesAndLadders.py
@@ -1,69 +1,58 @@
 from typing import List
 from heapq import *
+from collections import deque
 
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
-        # return self.recursiveSnakesAndLadders(board, 0, False)
         maxIndex = len(board) ** 2 - 1
-        minSteps = maxIndex
-        frontier = [(0, 0, False)]
-        startingRowMod = (len(board) - 1) % 2
+        frontier = deque()
+        frontier.append(0)
+        n = len(board)
+        startingRowMod = (n - 1) % 2
+        currentSteps = 0
+        newFrontier = deque()
+        visited = set()
         while (frontier):
-            current = heappop(frontier)
-            currentSteps = current[0]
-            currentStart = current[1]
-            currentDidSpecialMove = current[2]
+            currentStart = frontier.pop()
             if currentSteps > maxIndex:  return -1
-            newDidSpecialMove = False
             for i in range(1, 7):
                 nextStep = currentStart + i
-                if nextStep > maxIndex:  continue
-                # if not currentDidSpecialMove:
-                if True:
-                    rowIndex = len(board) - nextStep // len(board) - 1
-                    if rowIndex % 2 == startingRowMod:
-                    # if rowIndex % 2 != startingRowMod:
-                        colIndex = nextStep % len(board)
-                    else:
-                        colIndex = len(board) - (nextStep % len(board)) - 1
-                    nextValue = board[rowIndex][colIndex]
-                    if nextValue != -1:
-                        newDidSpecialMove = True
-                        nextStep = nextValue - 1
+                if nextStep in visited: continue
+                visited.add(nextStep)
+                if nextStep > maxIndex:  break
+                rowIndex = n - nextStep // n - 1
+                if rowIndex % 2 == startingRowMod:
+                    colIndex = nextStep % n
+                else:
+                    colIndex = n - (nextStep % n) - 1
+                nextValue = board[rowIndex][colIndex]
+                if nextValue != -1:
+                    nextStep = nextValue - 1
                 if nextStep == maxIndex:
                     return currentSteps + 1
-
-                newNode = (currentSteps + 1, nextStep, newDidSpecialMove)
-                if newNode not in frontier:
-                    heappush(frontier, newNode)
-
+                newFrontier.append(nextStep)
+                
+            if len(frontier) == 0:
+                frontier = newFrontier
+                newFrontier = deque()
+                currentSteps += 1
         return -1
-    
-    # def recursiveSnakesAndLadders(self, board, start, didSpecialMove):
-    #     maxIndex = (len(board) ** 2) - 1
-    #     minSteps = maxIndex
-    #     for i in range(1, 7):
-    #         nextStep = start + i
-    #         if nextStep > maxIndex:  continue
-    #         if nextStep == maxIndex:  return 1
-
-    #         newDidSpecialMove = False
-    #         if not didSpecialMove:
-    #             rowIndex = len(board) - nextStep // len(board) - 1
-    #             if rowIndex % 2 == 1:
-    #                 colIndex = nextStep % len(board)
-    #             else:
-    #                 colIndex = len(board) - nextStep % len(board) - 1
-    #             nextValue = board[rowIndex][colIndex]
-    #             if nextValue != -1:
-    #                 newDidSpecialMove = True
-    #                 nextStep = nextValue - 1
-    #         minSteps = min(minSteps, self.recursiveSnakesAndLadders(board, nextStep, newDidSpecialMove))
-    #     return minSteps
 
 def main():
     sol = Solution()
+    board = [[-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1],[-1,35,-1,-1,13,-1],[-1,-1,-1,-1,-1,-1],[-1,15,-1,-1,-1,-1]]
+    result = sol.snakesAndLadders(board)
+    print(result)
+    assert result == 4
+    board = [[-1,-1,-1],[-1,9,8],[-1,8,9]]
+    result = sol.snakesAndLadders(board)
+    print(result)
+    assert result == 1
     board = [[-1,-1,2,-1],[14,2,12,3],[4,9,1,11],[-1,2,1,16]]
+    result = sol.snakesAndLadders(board)
+    print(result)
+    assert result == 1
+    board = [[-1,-1],[-1,3]]
     result = sol.snakesAndLadders(board)
     print(result)
     assert result == 1
@@ -71,18 +60,6 @@ def main():
     result = sol.snakesAndLadders(board)
     print(result)
     assert result == -1
-    board = [[-1,-1,-1],[-1,9,8],[-1,8,9]]
-    result = sol.snakesAndLadders(board)
-    print(result)
-    assert result == 1
-    board = [[-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1],[-1,35,-1,-1,13,-1],[-1,-1,-1,-1,-1,-1],[-1,15,-1,-1,-1,-1]]
-    result = sol.snakesAndLadders(board)
-    print(result)
-    assert result == 4
-    board = [[-1,-1],[-1,3]]
-    result = sol.snakesAndLadders(board)
-    print(result)
-    assert result == 1
 
 main()
 
@@ -101,4 +78,30 @@ Simpler question: formula for swtiched colIndex
     reverse: 0 -> n - 1
             1 -> n - 2
             i -> n - i - 1
+
+My version is a lot slower than most
+    Worst case time complexity:
+        Unreachable
+        Need to go up to n^2 steps on every path
+    Can improve for some cases by checking if last square is directly accessible
+        Doesn't help with breaks that happen later though
+
+Better solution ideas:
+    Priority queue from end to start
+    Adjacency list
+
+Adjacency list:
+    Node adjacent to:
+        Any that directly reference it
+        i - 1, if i - 1 == -1
+
+Problem: for non-normal rolls, adjacency list doesn't read the intermediate but the starter
+    And that starter can be more then one
+    E.g. if a bunch can reach index k, which is a snake or ladder
+        Then the destination it goes to is actually connected by 1 step to the other ones
+            But how does a double jump work there?
+            By only including the direct one, you're covered
+                I.e. don't do it recursively
+
+Have a second adjacency list for get there by snake or ladder?
 '''
