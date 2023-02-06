beb220d6b5d9ce1d3fa18e98ef64f4fd3265a94f
Axirr <sherlock.scott@gmail.com>
2023-02-06 10:29:38 -0700

    Incomplete solution trying to do in place.

--- leetCode/javascript/shuffleTheArray.js
+++ leetCode/javascript/shuffleTheArray.js
@@ -3,6 +3,26 @@
  * @param {number} n
  * @return {number[]}
  */
+var NOTWORKINGshuffle = function(nums, n) {
+    let i = 1;
+    // let temp = nums[n+1];
+    let destination;
+    while (i < n) {
+        let temp = nums[n+i]
+        let firstDestination = 2 * i
+        let temp2 = nums[firstDestination]
+        nums[firstDestination] = temp
+        let secondDestination = i * 2 + 1
+        temp = nums[secondDestination]
+        nums[secondDestination] = temp2
+        let thirdDestination = i * 2 + 2
+
+
+        i += 1
+    }
+    return nums;
+}
+
 var shuffle = function(nums, n) {
     let resultList = [];
     let i;
@@ -58,4 +78,27 @@ Naive:
     Calc midpoint index
     Build array doing i, then i + midpoint
     Time: O(n)
+
+In place (to see if I can):
+    One temp variable won't do it
+    Instead, have to follow the index order?
+        E.g. 1 replaces x, then put x in y, then do y next, etc...
+    But that will just be alternating, so single temp should work
+
+Back half, subtract n to get place
+Front half, add current 2 * (count - 1) to get place
+    E.g. x3 has to moved forward by 2, because y1 and y2 newly in there
+        x1 and x2 also moved, but they were already in there
+
+That way seems awfully complicated
+
+y -> x then x -> y
+E.g. length 8
+    4 is first of back
+    4 -> 1
+    1 -> 2 * 1 -> 2
+    2 -> 4
+        Already used 4, increment to 5?
+    5 -> 2 * 1 + 1 -> 3
+    3 -> 6
 */
