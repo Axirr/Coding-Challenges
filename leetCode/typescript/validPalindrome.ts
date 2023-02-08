70f0949bfa482043d8f88fd32ef19b6ff1846c60
Axirr <sherlock.scott@gmail.com>
2023-02-08 11:49:40 -0700

    Solution to Valid Palindrome, in TypeScript.

---
+++ leetCode/typescript/validPalindrome.ts
@@ -0,0 +1,42 @@
+function isPalindrome(s: string): boolean {
+    let frontIndex:number = 0;
+    let backIndex:number = s.length - 1;
+
+    // Convert and remove
+    // s = s.toLowerCase();
+    let alphaRegEx:RegExp = new RegExp('[A-Za-z0-9]');
+
+    while (true) {
+        while (frontIndex < backIndex) {
+            if (s[frontIndex].match(alphaRegEx)) { break; }
+            frontIndex += 1;
+        }
+
+        while (frontIndex < backIndex) {
+            if (s[backIndex].match(alphaRegEx)) { break; }
+            backIndex -= 1;
+        }
+
+        if (frontIndex >= backIndex) { break; }
+        // console.log(s[frontIndex])
+        // console.log(s[backIndex])
+        // console.log()
+        if (s[frontIndex].toLowerCase() !== s[backIndex].toLowerCase()) { return false; }
+        frontIndex += 1;
+        backIndex -= 1;
+    }
+
+    return true;
+};
+
+function mainIsPalindrome(): void {
+    let s:string;
+    let boolIsPal:boolean;
+
+    s = "A man, a plan, a canal: Panama";
+    boolIsPal = isPalindrome(s);
+    console.log(boolIsPal);
+    console.assert(boolIsPal);
+}
+
+mainIsPalindrome();
