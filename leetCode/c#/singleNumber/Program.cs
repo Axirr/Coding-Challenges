39107455bb4aa70c8cec56c156b0c9e309ad8e2c
Axirr <sherlock.scott@gmail.com>
2023-02-09 12:13:49 -0700

    Solution for Single Number, in C#.

---
+++ leetCode/c#/singleNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+class Program {
+    static public void Main() {
+        Solution sol = new Solution();
+        int[] nums;
+        int resultInt;
+
+        nums = new int[] { 2, 2, 1 };
+        resultInt = sol.SingleNumber(nums);
+        Console.WriteLine(resultInt);
+        Debug.Assert(resultInt == 1);
+    }
+}
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        Dictionary<int, int> countDictionary = new Dictionary<int, int>();
+        foreach (int num in nums) {
+            if (countDictionary.ContainsKey(num)) {
+                countDictionary.Remove(num);
+            } else {countDictionary[num] = 1; }
+        }
+        int resultInt = nums[0];
+        foreach (var keyValuePair in countDictionary) {
+            if (keyValuePair.Value == 1) { 
+                resultInt = keyValuePair.Key;
+                break;
+            }
+        }
+        return resultInt;
+    }
+}
--- leetCode/c#/testingStuff/Program.cs
+++ leetCode/c#/testingStuff/Program.cs
@@ -44,5 +44,101 @@ class Program {
             where myInt % 2 == 0
             select myInt;
         foreach (int num  in evenQuery)  { Console.WriteLine(num); }
+
+        Card myCard = new Card(7, "Clubs");
+        Console.WriteLine($"{myCard.Rank} of {myCard.Suit}");
+        myCard.Rank = 9;
+        myCard.Suit = "Spaghetti";
+        Console.WriteLine($"{myCard.Rank} of {myCard.Suit}");
+
+        int mySwitchInt = 5;
+        switch (mySwitchInt) {
+            case 5:
+                Console.WriteLine("Num was 5");
+                break;
+            default:
+                Console.WriteLine("Default");
+                break;
+        }
+
+        Season mySeason = Season.Fall;
+        Console.WriteLine(mySeason);
+
+        Circle myCircle = new Circle(123);
+        Console.WriteLine(myCircle.LargestDimension);
+
+        Dog myDog = new Dog("brown");
+        Console.WriteLine(myDog.Color);
+        Console.WriteLine(myDog.HeartBeat);
+    }
+    enum Season {
+        Spring, Summer, Fall, Winter
+    }
+
+    class Card {
+        private int _rank;
+        private string _suit;
+
+        public Card(int rank, string suit) {
+            _rank = rank;
+            _suit = suit;
+        }
+
+        public int Rank {
+            get => _rank;
+            set {
+                _rank = value;
+            }
+        }
+        public string Suit {
+            get => _suit;
+            set {
+                _suit = value;
+            }
+        }
+    }
+
+    interface Shape {
+        abstract public int LargestDimension {
+            get;
+            set;
+        }
+    }
+
+    class Circle : Shape {
+        private int _radius;
+
+        public Circle(int radius) {
+            _radius = radius;
+        }
+
+        public int LargestDimension {
+            get => _radius;
+            set => _radius = value;
+        }
+    }
+
+    abstract class Animal {
+        public string HeartBeat {
+            get => "bum bum";
+        }
+
+        abstract public string Color {
+            get;
+            set;
+        }
+    }
+
+    class Dog : Animal {
+        private string _color;
+
+        public Dog(string color) {
+            _color = color;
+        }
+
+        public override string Color {
+            get => _color;
+            set => _color = value;
+        }
     }
 }
