cfae4c8010fff71551e617047dd84593d14a07cb
Axirr <sherlock.scott@gmail.com>
2023-02-09 12:28:17 -0700

    Solution for Excel Column Number, in C#.

---
+++ leetCode/c#/excelSheetColumnNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+class Program {
+    public static void Main() {
+        Solution sol = new Solution();
+        string columnTitle;
+        int colNum;
+        
+        columnTitle = "A";
+        colNum = sol.TitleToNumber(columnTitle);
+        Console.WriteLine(colNum);
+        Debug.Assert(colNum == 1);
+
+        columnTitle = "AB";
+        colNum = sol.TitleToNumber(columnTitle);
+        Console.WriteLine(colNum);
+        Debug.Assert(colNum == 28);
+    }
+}
+public class Solution {
+    public int TitleToNumber(string columnTitle) {
+        int colNum = 0;
+
+        int multiplier = (int)Math.Pow(26, (columnTitle.Length - 1));
+        foreach (char letter in columnTitle) {
+            colNum += multiplier * ((int)letter - 64);
+            multiplier /= 26;
+        }
+        // colNum += 26 * (columnTitle.Length - 1);
+
+        return colNum;
+    }
+}
