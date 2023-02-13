e8b48defac18a7919d5d7d8346c35976757214b6
Axirr <sherlock.scott@gmail.com>
2023-02-13 09:26:47 -0700

    Solution for Count Odd In Interval, in C#.

---
+++ leetCode/c#/countOddNumbersInInterval/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program {
+    static public void Main() {
+        Solution sol = new Solution();
+        int low;
+        int high;
+        int countOdd;
+
+        low = 3;
+        high = 7;
+        countOdd = sol.CountOdds(low, high);
+        Console.WriteLine(countOdd);
+        Debug.Assert(countOdd == 3);
+
+        low = 8;
+        high = 10;
+        countOdd = sol.CountOdds(low, high);
+        Console.WriteLine(countOdd);
+        Debug.Assert(countOdd == 1);
+    }
+}
+public class Solution {
+    public int CountOdds(int low, int high) {
+        if (low == high) {
+            return low % 2;
+        } if (low % 2 == 1 && high % 2 == 1) {
+            return (high - low) / 2 + high % 2 + low % 2 - 1;
+        }
+        int oddCount = ((high - low) / 2) + high % 2 + low % 2;
+        return oddCount;
+    }
+}
