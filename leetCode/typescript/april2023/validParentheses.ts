affcfdebacf3bc8a6ea9ae335885dae9236cfcb7
Axirr <sherlock.scott@gmail.com>
2023-04-10 10:48:34 -0600

    Solution for 'Valid Parentheses', in TypeScript.

---
+++ leetCode/typescript/april2023/validParentheses.ts
@@ -0,0 +1,82 @@
+import myAssert from "../march2023/Trie";
+
+function isValid(s: string): boolean {
+    let openStack:string[] = [];
+    let forwardBracketMapping:Map<string,string> = new Map();
+    forwardBracketMapping.set('(', ')');
+    forwardBracketMapping.set('[', ']');
+    forwardBracketMapping.set('{', '}');
+
+    let backwardBracketMapping:Map<string,string> = new Map();
+    backwardBracketMapping.set(')', '(');
+    backwardBracketMapping.set(']', '[');
+    backwardBracketMapping.set('}', '{');
+
+    for (const letter of s) {
+        if (forwardBracketMapping.has(letter)) {
+            openStack.push(letter);
+        } else if (backwardBracketMapping.has(letter)) {
+            if (openStack.length === 0)  return false;
+            if (openStack[openStack.length - 1] !== backwardBracketMapping.get(letter))  return false;
+            openStack.pop();
+        }
+    }
+
+    return openStack.length === 0;
+};
+
+function mainIsValid():void {
+    let s:string;
+    let result:boolean;
+    let doQuitIfAssertFails:boolean = true;
+
+    s = "()";
+    result = isValid(s);
+    console.log(`Final result ${result}`);
+    myAssert(result, doQuitIfAssertFails);
+
+    s = "()[]{}";
+    result = isValid(s);
+    console.log(`Final result ${result}`);
+    myAssert(result, doQuitIfAssertFails);
+
+    s = "(]";
+    result = isValid(s);
+    console.log(`Final result ${result}`);
+    myAssert(!result, doQuitIfAssertFails);
+}
+
+mainIsValid();
+
+/*
+Data range/assumptions:
+s length n: [1, 10^4]
+Can be ()[]{}
+*/
+
+/*
+Tests:
+n = 1
+n = 10^4
+mix of types
+mix of types where one closes a different kind but still invalid
+*/
+
+/*
+Ideas:
+
+Naive:
+    Stack for each type of parentheses
+    If close fund and corresponding stack empty, invalid
+    If get to end and all stacks not empty, invalid
+    Else valid
+
+Idea doesn't work because closing needs to happen to the last open bracket
+One universal stack
+*/
+
+/*
+Completion time (minutes):
+Question difficulty:
+How did it go (0 - 6):
+*/
