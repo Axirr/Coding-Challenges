dc3ad5c9e05389f02ad3b4b4d4b4a772a84270b6
Axirr <sherlock.scott@gmail.com>
2023-05-08 10:26:43 -0600

    Solution for 'Matrix Diagonal Sum', in TypeScript.

---
+++ leetCode/typescript/may2023/matrixDiagonalSum.ts
@@ -0,0 +1,86 @@
+import myAssert from "../march2023/Trie";
+
+function diagonalSum(mat: number[][]): number {
+    let totalSum:number = 0;
+    let currentX:number = 0;
+    let currentY:number = 0;
+    let dim:number = mat.length;
+    let halfIndex:number = Math.floor(dim / 2);
+
+
+    let doCheck:boolean = false;
+    if (dim % 2 !== 0)  doCheck = true;
+
+    while (currentX < dim && currentY < dim) {
+        totalSum += mat[currentX][currentY];
+        let reverseY:number = dim - 1 - currentY
+        if (!doCheck || currentX !== halfIndex || reverseY !== halfIndex)  totalSum += mat[currentX][reverseY];
+
+        currentX++;
+        currentY++;
+    }
+
+    return totalSum;
+};
+
+function mainMatrixDiagonalSum():void {
+    let mat:number[][];
+    let result:number;
+    let doQuitIfAssertFails:boolean = true;
+
+    mat = [[7,3,1,9],[3,4,6,9],[6,9,6,6],[9,5,8,5]];
+    result = diagonalSum(mat);
+    console.log(`Final result ${result}`);
+    myAssert(result === 55, doQuitIfAssertFails);
+
+
+    mat = [[1,2,3],[4,5,6],[7,8,9]];
+    result = diagonalSum(mat);
+    console.log(`Final result ${result}`);
+    myAssert(result === 25, doQuitIfAssertFails);
+
+    mat = [[1,1,1,1],
+              [1,1,1,1],
+              [1,1,1,1],
+              [1,1,1,1]]
+    result = diagonalSum(mat);
+    console.log(`Final result ${result}`);
+    myAssert(result === 8, doQuitIfAssertFails);
+
+    mat = [[5]]
+    result = diagonalSum(mat);
+    console.log(`Final result ${result}`);
+    myAssert(result === 5, doQuitIfAssertFails);
+}
+
+mainMatrixDiagonalSum();
+
+/*
+Data range/assumptions:
+*/
+
+/*
+Tests:
+*/
+
+/*
+Ideas:
+
+Naive:
+
+Intersection of diagonals coordinates
+    Square matrices, so will happen for odd but not for even
+        E.g. [1,2]
+             [3, 4]
+            Shares nothing
+    At the half way point for odds
+    Odd, 
+*/
+
+/*
+Completion time (minutes): 26
+Question difficulty: Easy
+How did it go (0 - 6): 2
+    Question seemed hard
+    Bug that took me a while to find that was pretty simple
+*/
