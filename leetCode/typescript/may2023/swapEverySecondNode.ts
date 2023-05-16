392c96d20e8dcf86d5f59d887d239fd987bc6853
Axirr <sherlock.scott@gmail.com>
2023-05-16 10:08:53 -0600

    Solution for 'Swap Every Second Node', in TypeScript.

---
+++ leetCode/typescript/may2023/swapEverySecondNode.ts
@@ -0,0 +1,152 @@
+import myAssert from "../march2023/Trie";
+
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
+
+
+function swapPairs(head: ListNode | null): ListNode | null {
+    if (head === null || head.next === null)  return head;
+
+    let evenList:ListNode[] = [head];
+    let oddList:ListNode[] = [];
+    let currentNode:ListNode | null = head.next;
+    let count = 1;
+
+    while (currentNode !== null) {
+        if (count % 2 === 1)  oddList.push(currentNode);
+        else evenList.push(currentNode);
+
+        currentNode = currentNode.next;
+        count++;
+    }
+
+    let takeOdd:boolean = false;
+    let resultList:ListNode = oddList[0];
+    let currentResultNode:ListNode = resultList;
+    let oddIndex:number = 1;
+    let evenIndex:number = 0;
+    let nodeToAdd:ListNode;
+
+
+    while (true) {
+        if (oddIndex <= evenIndex && oddIndex < oddList.length) {
+            nodeToAdd = oddList[oddIndex];
+            oddIndex++;
+        } else {
+            nodeToAdd = evenList[evenIndex];
+            evenIndex++;
+        }
+
+        currentResultNode.next = nodeToAdd;
+
+        if (oddIndex === oddList.length && evenIndex === evenList.length) {
+            currentResultNode.next.next = null;
+            break;
+        }
+
+        currentResultNode = currentResultNode.next;
+        takeOdd = !takeOdd;
+    }
+
+    return resultList;
+};
+
+function mainSwapPairs(): void {
+    let head:ListNode | null;
+    let resultHead:ListNode | null;
+    let doQuitIfAssertFails:boolean = true;
+    let listResult:number[];
+    let correctList:number[];
+
+    head = linkedListFromList([1,2,3,4]);
+    resultHead = swapPairs(head);
+    listResult = listFromLinkedList(resultHead);
+    console.log(listResult);
+    correctList = [2,1,4,3];
+    myAssert(listEquality(listResult, correctList), doQuitIfAssertFails);
+
+    head = linkedListFromList([]);
+    resultHead = swapPairs(head);
+    listResult = listFromLinkedList(resultHead);
+    console.log(listResult);
+    correctList = [];
+    myAssert(listEquality(listResult, correctList), doQuitIfAssertFails);
+
+    head = linkedListFromList([1,2,3]);
+    resultHead = swapPairs(head);
+    listResult = listFromLinkedList(resultHead);
+    console.log(listResult);
+    correctList = [2, 1, 3];
+    myAssert(listEquality(listResult, correctList), doQuitIfAssertFails);
+}
+
+function linkedListFromList(list:number[]):ListNode | null {
+    if (list.length === 0)  return null;
+    let head:ListNode = new ListNode(list[0], null);
+    let previousNode:ListNode = head;
+    for (let i = 1; i < list.length; i++) {
+        const newNode = new ListNode(list[i], null);
+        previousNode.next = newNode;
+        previousNode = newNode;
+    }
+
+    return head;
+}
+
+function listFromLinkedList(linkedList:ListNode | null) {
+    let list:number[] = [];
+    let currentNode:ListNode | null = linkedList;
+
+    while (currentNode !== null) {
+        list.push(currentNode.val);
+        currentNode = currentNode.next;
+    }
+
+    return list;
+}
+
+function listEquality(list1:number[], list2:number[]) {
+    if (list1.length !== list2.length)  return false;
+    for (let i = 0; i < list1.length; i++) {
+        if (list1[i] !== list2[i])  return false;
+    }
+
+    return true;
+}
+
+mainSwapPairs();
+
+/*
+Data range/assumptions:
+n: [0, 100]
+val: [0, 100]
+*/
+
+/*
+Tests:
+n = 1
+n = 100
+n = 2
+*/
+
+/*
+Ideas:
+
+Naive:
+    Make separate lists of both
+    Traverse one, insert, until non-head list is consumed
+*/
+
+/*
+Completion time (minutes): 25
+Question difficulty: Medium
+How did it go (1 - 6): 3
+    Idea was good
+    Had some tricky bugs though
+*/
--- leetCode/typescript/may2023/swapKthBeginningEnd.ts
+++ leetCode/typescript/may2023/swapKthBeginningEnd.ts
@@ -1,3 +1,5 @@
+import myAssert from "../march2023/Trie"
+
 class ListNode {
     val: number
     next: ListNode | null
@@ -7,89 +9,205 @@ class ListNode {
     }
 
     prettyPrint(count = 0) {
-        if (count > 15)  return
+        if (count > 15)  throw new Error("likely infinite loop")
         console.log(this.val);
         if (this.next !== null)  this.next.prettyPrint(count + 1);
     }
 }
 
 
+// function valueSwapNodes(head: ListNode | null, k: number): ListNode | null {
+//     let count:number = 0;
+//     if (head === null) throw new Error("head is null");
+//     let currentNode:ListNode = head!;
+//     let kStartParentNode:ListNode | null = null;
+//     let kEndParentNode:ListNode = head;
+
+//     while (true) {
+//         if (count === k - 1)  kStartParentNode = currentNode;
+//         if (count > k - 1) kEndParentNode = kEndParentNode.next!;
+
+//         count += 1;
+//         if (currentNode.next === null)  break
+//         else currentNode = currentNode.next;
+//     }
+
+//     let tempStoreVal:number = kStartParentNode!.val;
+//     kStartParentNode!.val = kEndParentNode.val;
+//     kEndParentNode.val = tempStoreVal;
+
+//     return head;
+// }
+
 function swapNodes(head: ListNode | null, k: number): ListNode | null {
     let count:number = 0;
     if (head === null) throw new Error("head is null");
-    let currentNode:ListNode = head!;
-    let kStartParentNode:ListNode | null = null;
-    let kEndParentNode:ListNode = head;
-
-    while (true) {
-        if (count === k - 1)  kStartParentNode = currentNode;
-        if (count > k - 1) kEndParentNode = kEndParentNode.next!;
-
-        count += 1;
-        if (currentNode.next === null)  break
-        else currentNode = currentNode.next;
+    if (k === 1 && head.next === null)  return head;
+    if (head.next!.next === null)  {
+        let secondNode:ListNode = new ListNode(head.val, null);
+        return new ListNode(head.next!.val, secondNode);
     }
 
-    let tempStoreVal:number = kStartParentNode!.val;
-    kStartParentNode!.val = kEndParentNode.val;
-    kEndParentNode.val = tempStoreVal;
-
-    return head;
-}
-
-function complicatedSwwapNodes(head: ListNode | null, k: number): ListNode | null {
-    let count:number = 0;
-    if (head === null) throw new Error("head is null");
     let currentNode:ListNode = head!;
     let kStartParentNode:ListNode | null = null;
-    let kEndParentNode:ListNode = head;
+    let kEndParentNode:ListNode | null = head;
+    let didChange:boolean = false
+    let maybeValue:ListNode | null = null;
 
     while (true) {
         if (count === k - 2)  kStartParentNode = currentNode;
-        if (count > k) kEndParentNode = kEndParentNode.next!;
+        if (count > k) {
+            kEndParentNode = kEndParentNode.next!;
+            didChange = true;
+        }
+        if (count === k - 2 && !didChange) maybeValue = currentNode;
 
         count += 1;
-        if (currentNode.next === null)  break
+        if (currentNode.next === null)  {
+            break
+        }
         else currentNode = currentNode.next;
     }
+
+    if (!didChange)  {
+        kEndParentNode = maybeValue;
+    }
+
     if (kStartParentNode !== null) console.log(`Start parent val is ${kStartParentNode.val}`)
     else  console.log(`START IS NULL`)
-    console.log(`End parent val is ${kEndParentNode.val}`)
+    if (kEndParentNode !== null)  console.log(`End parent val is ${kEndParentNode.val}`)
+    else  console.log(`END IS NULL`)
 
-    let kStartNode:ListNode;
-    if (kStartParentNode === null)  kStartNode = head;
-    else  kStartNode = kStartParentNode.next!;
-    let kEndNode:ListNode = kEndParentNode.next!;
+    if (kStartParentNode === kEndParentNode)  return head;
 
-    let swapNext:ListNode | null = kStartNode.next;
-    kStartNode.next = kEndNode.next;
-    kEndNode.next = swapNext;
+    let kStartNode:ListNode = head;
+    if (kStartParentNode !== null)  kStartNode = kStartParentNode.next!;
+    let kEndNode:ListNode = head;
+    if (kEndParentNode !== null)  kEndNode = kEndParentNode.next!;
 
-    kEndParentNode.next = kStartNode;
-    kStartParentNode!.next = kEndNode;
-    // if (kStartParentNode !== null)  kStartParentNode.next = kEndNode;
-    // else return kEndNode;
+    // let swapNext:ListNode | null = kStartNode.next;
+    // kStartNode.next = kEndNode.next;
+    // kEndNode.next = swapNext;
+
+    let distance:number = Math.abs((k - 2) - (count - k - 1));
+    // console.log(`Distance ${distance}`)
+    // if (distance === 1 && (kStartParentNode !== null || kEndParentNode !== null))  {
+    if (distance === 1) {
+        let absoluteFirst:ListNode = kStartParentNode!;
+        let absoluteLast:ListNode | null = kEndNode!.next;
+        let firstValue:number = kStartNode.val;
+        let secondValue:number = kEndNode.val;
+
+        if ((k - 2) >  count - k - 1) {
+            absoluteFirst = kEndParentNode!;
+            absoluteLast = kStartNode!.next;
+            firstValue = kEndNode.val;
+            secondValue = kStartNode.val;
+        }
+
+        let secondNew = new ListNode(firstValue, absoluteLast);
+        let firstNew = new ListNode(secondValue, secondNew);
+        if (absoluteFirst !== null) absoluteFirst.next = firstNew;
+        else return firstNew
+
+        return head;
+    }
+
+    let swapNext:ListNode | null = kEndNode.next;
+    kEndNode.next = kStartNode.next;
+    kStartNode.next = swapNext;
+
+    if (kEndParentNode !== null)  kEndParentNode.next = kStartNode;
+    if (kStartParentNode !== null)  kStartParentNode!.next = kEndNode;
 
 
-    // if (k === 0)  return kEndNode;
+    console.log(`${count - k}`)
+    if (k === 1 || count - k === 0)  {
+        console.log('Different return')
+        return kEndNode;
+    }
+
     return head;
-};
+}
 
 function mainSwapNodes():void {
     let head:ListNode | null;
     let k:number;
     let result:ListNode | null;
+    let listResult:number[];
+    let correctList:number[];
+    let doQuitIfAssertFails:boolean = true;
+
+    head = linkedListFromList([1]);
+    k = 1;
+    result = swapNodes(head, k);
+    listResult = listFromLinkedList(result);
+    correctList = [1];
+    console.log(listResult);
+    myAssert(listEquality(listResult, correctList), doQuitIfAssertFails);
+    console.log()
+
+    head = linkedListFromList([100,24,24,36,18,52,95,61,54,88,86,79,11,1,31,26]);
+    k = 16;
+    result = swapNodes(head, k);
+    listResult = listFromLinkedList(result);
+    correctList = [26,24,24,36,18,52,95,61,54,88,86,79,11,1,31,100];
+    console.log(listResult);
+    myAssert(listEquality(listResult, correctList), doQuitIfAssertFails);
+    console.log()
+
+    head = linkedListFromList([80,46,66,35,64]);
+    k = 1;
+    result = swapNodes(head, k);
+    listResult = listFromLinkedList(result);
+    correctList = [64,46,66,35,80];
+    console.log(listResult);
+    myAssert(listEquality(listResult, correctList), doQuitIfAssertFails);
+    console.log()
+
+    head = linkedListFromList([100,90]);
+    k = 2;
+    result = swapNodes(head, k);
+    listResult = listFromLinkedList(result);
+    correctList = [90, 100];
+    console.log(listResult);
+    myAssert(listEquality(listResult, correctList), doQuitIfAssertFails);
+    console.log()
+
+    head = linkedListFromList([1,2]);
+    k = 1;
+    result = swapNodes(head, k);
+    listResult = listFromLinkedList(result);
+    correctList = [2,1];
+    console.log(listResult);
+    myAssert(listEquality(listResult, correctList), doQuitIfAssertFails);
+    console.log()
 
     head = linkedListFromList([7,9,6,6,7,8,3,0,9,5]);
     k = 5;
     result = swapNodes(head, k);
-    result!.prettyPrint();
+    listResult = listFromLinkedList(result);
+    correctList = [7,9,6,6,8,7,3,0,9,5];
+    console.log(listResult);
+    myAssert(listEquality(listResult, correctList), doQuitIfAssertFails);
+    console.log()
+
+    head = linkedListFromList([7,9,6,6,7,8,3,0,9,5]);
+    k = 6;
+    result = swapNodes(head, k);
+    listResult = listFromLinkedList(result);
+    correctList = [7,9,6,6,8,7,3,0,9,5];
+    console.log(listResult);
+    myAssert(listEquality(listResult, correctList), doQuitIfAssertFails);
     console.log()
 
     head = linkedListFromList([1,2,3,4,5]);
     k = 2;
     result = swapNodes(head, k);
-    result!.prettyPrint();
+    listResult = listFromLinkedList(result);
+    correctList = [1,4,3,2,5];
+    console.log(listResult);
+    myAssert(listEquality(listResult, correctList), doQuitIfAssertFails);
     console.log()
 }
 
@@ -104,6 +222,27 @@ function linkedListFromList(list:number[]):ListNode {
     }
 
     return head;
+}
+
+function listFromLinkedList(linkedList:ListNode | null) {
+    let list:number[] = [];
+    let currentNode:ListNode | null = linkedList;
+
+    while (currentNode !== null) {
+        list.push(currentNode.val);
+        currentNode = currentNode.next;
+    }
+
+    return list;
+}
+
+function listEquality(list1:number[], list2:number[]) {
+    if (list1.length !== list2.length)  return false;
+    for (let i = 0; i < list1.length; i++) {
+        if (list1[i] !== list2[i])  return false;
+    }
+
+    return true;
 }
 
 mainSwapNodes();
@@ -138,6 +277,32 @@ Naive:
 
 What to do in case where parent is the one being swapped?
     Move parent value back one?
+
+Two situations to deal with:
+    A parent node is null (i.e. one of our nodes is the head)
+    One of our nodes is the parent of the other
+
+Node is head:
+    Skip next swap on that parent
+    Return the new head
+    For special case k = 1 and n = 1, just return list no changes
+
+Parent is a swap target:
+    Move parent back one after getting the swapNode from it?
+        Non-trivial with non-linked list
+        Recursively run with k - 1?
+        Seems ugly
+
+Change swapping order?
+    If we swap the swap target first, and update parent of other, will be fixed?
+    What about special case both have same parent?
+        Just return list, no swap needed
+    Nope
+
+Special procedure to just redo that part entirely?
+    Use end of latest and start of earliest
+    Make new nodes with new values
+    Fresh nodes
 */
 
 /*
