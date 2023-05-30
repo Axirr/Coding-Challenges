989cff54bc8f681eae2c978744ce281c1626bc42
Axirr <sherlock.scott@gmail.com>
2023-05-30 11:44:56 -0600

    Improved solution for 'Design Hash Set', in TypeScript.

--- leetCode/typescript/may2023/designHashSet.ts
+++ leetCode/typescript/may2023/designHashSet.ts
@@ -1,41 +1,81 @@
 class MyHashSet {
-    myArray:Array<number | null>;
+    MAX_HASH_SIZE = 10000;
+    myArray:Array<Array<number>>;
 
     constructor() {
-        this.myArray = Array(64).fill(null);
+        this.myArray = Array(64).fill([]);
     }
 
     add(key: number): void {
+        if (this.contains(key)) return;
+
         let hashKey = this.hashFunction(key);
-        if (hashKey > this.myArray.length) {
+        if (hashKey >= this.myArray.length) {
             let newLength:number = this.myArray.length;
             while (newLength < hashKey) {
                 newLength*=2;
             }
-            newLength = Math.min(newLength, 10**6);
-            let newArray = Array(newLength).fill(null);
+            newLength = Math.min(newLength, this.MAX_HASH_SIZE);
+            let newArray = Array(newLength).fill([]);
             for (let index = 0; index < this.myArray.length; index++) {
                 newArray[index] = this.myArray[index];
             }
             this.myArray = newArray;
         }
-        this.myArray[this.hashFunction(key)] = key;
+
+        let arrayForKey:Array<number> = this.myArray[hashKey];
+        if (arrayForKey.length === 0) {
+            arrayForKey.push(key);
+            return;
+        }
+
+        let insertionIndex:number = this.binarySearch(arrayForKey, key, true);
+        arrayForKey.splice(insertionIndex, 0, key);
     }
 
     remove(key: number): void {
         let hashKey:number = this.hashFunction(key);
         if (hashKey >= this.myArray.length)  return;
-        this.myArray[this.hashFunction(key)] = null;
+        let arrayForKey:number[] = this.myArray[hashKey];
+        if (arrayForKey.length === 0)  return;
+        let searchIndex:number = this.binarySearch(arrayForKey, key);
+        if (searchIndex === -1)  return;
+        // Slow (O(n)), would be nice to improve
+        //      n should be bounded by collision factor of hash
+        arrayForKey.splice(searchIndex, 1);
     }
 
     contains(key: number): boolean {
         let hashKey:number = this.hashFunction(key);
         if (hashKey >= this.myArray.length)  return false;
-        return this.myArray[this.hashFunction(key)] !== null;
+        let arrayForKey:number[] = this.myArray[hashKey];
+        let searchIndex:number = this.binarySearch(arrayForKey, key);
+        return searchIndex !== -1;
     }
 
+    // Not sure if this is actually a good hash function
     hashFunction(key:number): number {
-        return key;
+        let firstPrime:number = 1618931;
+        let secondPrime:number = 1233527;
+        return ((key + firstPrime) % this.MAX_HASH_SIZE + secondPrime) % this.MAX_HASH_SIZE;
+    }
+
+    binarySearch(collisionArray:number[], key:number, isInsert:boolean = false):number {
+        let low:number = 0;
+        let high:number = collisionArray.length - 1;
+        let middle = Math.ceil((low + high) / 2);
+        while (low <= high) {
+            middle = Math.ceil((low + high) / 2);
+            if (collisionArray[middle] === key)  return middle;
+            else if (collisionArray[middle] > key)  high = middle - 1;
+            else low = middle + 1;
+        }
+        middle = Math.ceil((low + high) / 2);
+        if (isInsert)  {
+            // console.log(`middle for insertion ${middle}`)
+            return middle;
+        }
+        return -1;
     }
 }
 
@@ -55,6 +95,20 @@ function mainMyHashSet():void {
     myHashSet.add(81);
     console.log(myHashSet.contains(7));
     console.log(myHashSet.contains(81));
+    myHashSet.add(81);
+
+    let currentSize:number = 10000;
+    let maxSize:number = 1000000;
+    let divisor:number = Math.ceil(maxSize / currentSize);
+    let bucketCount:number[] = Array(divisor).fill(0);
+    let attempts:number = 10000000;
+    for (let i = 0; i < attempts; i++) {
+        let randNum:number = Math.floor(Math.random() * (maxSize - 1));
+        let hashKey:number = myHashSet.hashFunction(randNum);
+        let bucketKey:number = hashKey % divisor;
+        bucketCount[bucketKey] = bucketCount[bucketKey] + 1;
+    }
+    console.log(bucketCount);
 }
 
 mainMyHashSet();
