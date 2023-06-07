d2542714ab7002e13fc39bccf07d9a81a1b50ed0
Axirr <sherlock.scott@gmail.com>
2023-06-07 10:38:14 -0600

    Actual solution for 'Minimum Bit Flips', in TypeScript.

--- leetCode/typescript/june2023/minFlipsMakeEqual.ts
+++ leetCode/typescript/june2023/minFlipsMakeEqual.ts
@@ -1,24 +1,28 @@
 import myAssert from "../march2023/Trie";
 
-function minimumFlipsMakeEqual(a:number, b:number, c:number):number {
-    let tupArgs:[number, number][] = [[a,b],[b,c],[a,c]];
+function minFlips(a:number, b:number, c:number):number {
+    let orAB:number = a | b;
+    let andAB:number = a & b;
+    let xorAB = orAB ^ c;
+    let xorCount:number = 0;
+    while (xorAB > 0) {
+        let cBit:number = c % 2;
+        let orBit:number = xorAB % 2;
+        let andBit:number = andAB % 2;
 
-    let minFlips:number | null = null;
-    for (const permutation of tupArgs) {
-        let first:number = permutation[0];
-        let second:number = permutation[1];
-        let xorAB:number = first ^ second;
-        let xorCount:number = 0;
-        while (xorAB > 0) {
+        if (cBit === 0) {
+            if (andBit === 1)  xorCount += 2;
+            else if (orBit === 1) xorCount += 1;
+        } else {
             xorCount += xorAB % 2;
-            xorAB = xorAB >> 1;
         }
 
-        if (minFlips === null)  minFlips = xorCount;
-        else  minFlips = Math.min(minFlips, xorCount);
+        xorAB = xorAB >> 1;
+        c = c >> 1;
+        andAB = andAB >> 1;
     }
 
-    return minFlips!;
+    return xorCount;
 }
 
 function mainMinFlipsMakeEqaul():void {
@@ -26,47 +30,65 @@ function mainMinFlipsMakeEqaul():void {
     let b:number;
     let c:number;
     let result:number;
-    let doQuitIfAssertFails:boolean = true;
+    let doQuitIfAssertFails:boolean = false;
+
+    a = 2;
+    b = 6;
+    c = 5;
+    result = minFlips(a, b, c);
+    console.log(`final result ${result}`);
+    myAssert(result === 3, doQuitIfAssertFails);
+
+    a = 4, b = 2, c = 7;
+    result = minFlips(a, b, c);
+    console.log(`final result ${result}`);
+    myAssert(result === 1, doQuitIfAssertFails);
+
+    a = 1, b = 2, c = 3;
+    result = minFlips(a, b, c);
+    console.log(`final result ${result}`);
+    myAssert(result === 0, doQuitIfAssertFails);
+    return
 
     a = 3;
     b = 0;
     c = 1;
-    result = minimumFlipsMakeEqual(a, b, c);
+    result = minFlips(a, b, c);
     console.log(`final result ${result}`);
     myAssert(result === 1, doQuitIfAssertFails);
 
     a = 1024;
     b = 5;
     c = 5;
-    result = minimumFlipsMakeEqual(a, b, c);
+    result = minFlips(a, b, c);
     console.log(`final result ${result}`);
     myAssert(result === 0, doQuitIfAssertFails);
 
     a = 9;
     b = 9;
     c = 333;
-    result = minimumFlipsMakeEqual(a, b, c);
+    result = minFlips(a, b, c);
     console.log(`final result ${result}`);
     myAssert(result === 0, doQuitIfAssertFails);
 
     a = -5;
     b = 9;
     c = -5;
-    result = minimumFlipsMakeEqual(a, b, c);
+    result = minFlips(a, b, c);
     console.log(`final result ${result}`);
     myAssert(result === 0, doQuitIfAssertFails);
 
     a = 7;
     b = 0;
     c = 13456789;
-    result = minimumFlipsMakeEqual(a, b, c);
+    result = minFlips(a, b, c);
     console.log(`final result ${result}`);
     myAssert(result === 3, doQuitIfAssertFails);
     
     a = 15;
     b = 16;
     c = 3;
-    result = minimumFlipsMakeEqual(a, b, c);
+    result = minFlips(a, b, c);
     console.log(`final result ${result}`);
     myAssert(result === 2, doQuitIfAssertFails);
 }
